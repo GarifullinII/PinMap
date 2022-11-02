3f2fc9158d79a60d0ff810d34255da1a314edf88
GArifullinII <ildaridrisovich@gmail.com>
2022-11-02 16:09:23 +0400

    Initial Commit

---
+++ PinMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinMap
+//
+//  Created by Ildar Garifullin on 02/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
